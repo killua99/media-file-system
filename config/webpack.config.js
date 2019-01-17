b3a2c7b8779ad7663203bea3228dc1f193e61a99
Luigi Guevara <luigi.fox15@gmail.com>
2019-01-17 23:40:59 +0100

    Webpack config updated

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,11 +1,33 @@
 'use strict'
 
 const path = require( 'path' )
+const glob = require( 'glob' )
+const env = process.env.NODE_ENV
+
+function toObject ( paths ) {
+	let ret = {}
+
+	paths.forEach( function ( path ) {
+		ret[ path.split( '/' ).slice( -1 )[ 0 ] ] = path
+	} )
+
+	return ret
+}
 
 module.exports = {
-	entry: () => './src',
+	mode: env || 'development',
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: 'babel-loader',
+			},
+		],
+	},
+	entry: toObject( glob.sync( path.resolve( __dirname, '../src' ) + '/**/*.js' ) ),
 	output: {
-		path: path.resolve( __dirname, 'assets' ),
-		filename: '[name].js',
+		path: path.resolve( __dirname, '../assets/js' ),
+		filename: '[name]',
 	},
 }
